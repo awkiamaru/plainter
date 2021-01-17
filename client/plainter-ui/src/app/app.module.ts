822903728b1a5b69f7e0817f46304c8342a83341
Luis Fernando <luis.fernando@cerc.inf.br>
2021-01-17 04:33:59 -0300

    feat: create header and footer page, and add fonts

--- client/plainter-ui/src/app/app.module.ts
+++ client/plainter-ui/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
