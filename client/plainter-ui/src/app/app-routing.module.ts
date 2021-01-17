9a570cba71deb6f691bff6e40af72d8c5a793776
Luis Fernando <luis.fernando@cerc.inf.br>
2021-01-17 07:23:16 -0300

    feat: Create home page and footer component

--- client/plainter-ui/src/app/app-routing.module.ts
+++ client/plainter-ui/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContentComponent } from './layout/content/content.component';
+import { HomeComponent } from './layout/content/home/home.component';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: ContentComponent,
+    component: HomeComponent,
   }
 ];
 
